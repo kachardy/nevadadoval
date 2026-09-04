6d9328518b48de233ab05b1ea31ce82fd8f23f86
kachardy <kauerichardy138@gmail.com>
2026-09-04 17:24:50 -0300

    fix: consertando deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,16 @@
 // src/App.tsx
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { Cardapio } from './pages/Cardapio';
 import { Admin } from './pages/Admin';
 
-function App() {
+export function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
-        {/* Rota raiz: O que o cliente vê ao acessar o domínio principal */}
         <Route path="/" element={<Cardapio />} />
-        
-        {/* Rota protegida: Onde o dono da loja vai gerenciar o sistema */}
         <Route path="/admin" element={<Admin />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
 
